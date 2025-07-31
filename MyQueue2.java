99d927b3f7b23986db46ea381367b3a1a425638c
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-07-31 23:18:07 +0530

    Create MyQueue2.java

---
+++ MyQueue2.java
@@ -0,0 +1,42 @@
+class MyQueue2 {
+
+    Stack<Integer> q1;
+    Stack<Integer> q2;
+
+    public MyQueue() {
+        q1 = new Stack<>();
+        q2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        while (q1.size() > 0 ){
+            q2.push(q1.pop());
+        }
+        q1.push(x);
+
+        while(q2.size()>0){
+            q1.push(q2.pop());
+        }
+    }
+    
+    public int pop() {
+        return q1.pop();
+    }
+    
+    public int peek() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.size() == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
